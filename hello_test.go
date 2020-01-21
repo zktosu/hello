f06400d989b6a357e64ffd7b711e8997faa18acb
zktosu <zktosu@gmail.com>
2020-01-21 18:38:46 +0300

    added parameter for name

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T){
-	got := Hello()
-	want := "Hello World!"
+	got := Hello("mert")
+	want := "Hello mert!"
 
 	if got != want {
 		t.Errorf("Got %q want %q", got, want)
